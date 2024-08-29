9986ac7585bacecb7ba3a04f9bf4dbd9f419e584
Simon Risk <76711152+SimonNRisk@users.noreply.github.com>
2024-08-29 17:09:23 -0230

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -1,149 +1,127 @@
 import os
 import librosa
-import librosa.display
 import pandas as pd
 from pydub import AudioSegment
-import tempfile
 import numpy as np
-import matplotlib.pyplot as plt
+import soundfile as sf
 
-# Function to convert MP3 to WAV, not used as of June 7
-def convert_mp3_to_wav(mp3_file):
-    sound = AudioSegment.from_mp3(mp3_file) #sound = the object taken from the mp3
-    wav_file = tempfile.mktemp(suffix='.wav') #creates new temporary file with suffix.wav
-    sound.export(wav_file, format="wav") #exports sound to wave_file under wav format
-    return wav_file
+#function to extract features from an audio segment
+def extract_features_from_segment(y, sr, start_time, end_time):
+    segment = y[start_time:end_time]
 
-# Function to extract features from an audio file for LSTM
-def extract_features(audio_file, sr=22050):
+    chroma_stft = librosa.feature.chroma_stft(y=segment, sr=sr)
+    rms = librosa.feature.rms(y=segment)
+    spectral_centroid = librosa.feature.spectral_centroid(y=segment, sr=sr)
+    spectral_bandwidth = librosa.feature.spectral_bandwidth(y=segment, sr=sr)
+    rolloff = librosa.feature.spectral_rolloff(y=segment, sr=sr)
+    zero_crossing_rate = librosa.feature.zero_crossing_rate(y=segment)
+    harmony, perceptr = librosa.effects.hpss(segment)
+    tempo, _ = librosa.beat.beat_track(y=segment, sr=sr)
+    mfcc = librosa.feature.mfcc(y=segment, sr=sr, n_mfcc=20)
+
+    features = {
+        'chroma_stft_mean': chroma_stft.mean() if chroma_stft.size else 0,
+        'chroma_stft_var': chroma_stft.var() if chroma_stft.size else 0,
+        'rms_mean': rms.mean() if rms.size else 0,
+        'rms_var': rms.var() if rms.size else 0,
+        'spectral_centroid_mean': spectral_centroid.mean() if spectral_centroid.size else 0,
+        'spectral_centroid_var': spectral_centroid.var() if spectral_centroid.size else 0,
+        'spectral_bandwidth_mean': spectral_bandwidth.mean() if spectral_bandwidth.size else 0,
+        'spectral_bandwidth_var': spectral_bandwidth.var() if spectral_bandwidth.size else 0,
+        'rolloff_mean': rolloff.mean() if rolloff.size else 0,
+        'rolloff_var': rolloff.var() if rolloff.size else 0,
+        'zero_crossing_rate_mean': zero_crossing_rate.mean() if zero_crossing_rate.size else 0,
+        'zero_crossing_rate_var': zero_crossing_rate.var() if zero_crossing_rate.size else 0,
+        'harmony_mean': harmony.mean() if harmony.size else 0,
+        'harmony_var': harmony.var() if harmony.size else 0,
+        'perceptr_mean': perceptr.mean() if perceptr.size else 0,
+        'perceptr_var': perceptr.var() if perceptr.size else 0,
+        'tempo': tempo,
+    }
+
+    for i in range(1, 21):
+        features[f'mfcc{i}_mean'] = mfcc[i-1].mean() if mfcc.shape[0] >= i else 0
+        features[f'mfcc{i}_var'] = mfcc[i-1].var() if mfcc.shape[0] >= i else 0
+
+    return features
+
+#function to load audio file
+def load_audio(file_path):
     try:
-        # Load the audio file with the desired sampling rate
-        y, sr = librosa.load(audio_file, sr=sr)
+        y, sr = librosa.load(file_path, sr=None)
+    except sf.LibsndfileError:
+        print(f"LibsndfileError: {file_path}")
+        return None, None
+    except Exception as e:
+        print(f"Error loading {file_path}: {e}")
+        return None, None
+    return y, sr
 
-        # Compute features over time
-        chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
-        rms = librosa.feature.rms(y=y)
-        spectral_centroid = librosa.feature.spectral_centroid(y=y, sr=sr)
-        spectral_bandwidth = librosa.feature.spectral_bandwidth(y=y, sr=sr)
-        rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
-        zero_crossing_rate = librosa.feature.zero_crossing_rate(y)
-        mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20)
+#function to extract features from an audio file
+def extract_features(audio_file, segment_duration=1):
+    try:
+        y, sr = load_audio(audio_file)
+        if y is None:
+            return []
 
-        # Combine features into a dictionary
-        features = {
-            'chroma_stft': chroma_stft.T,
-            'rms': rms.T,
-            'spectral_centroid': spectral_centroid.T,
-            'spectral_bandwidth': spectral_bandwidth.T,
-            'rolloff': rolloff.T,
-            'zero_crossing_rate': zero_crossing_rate.T,
-            'mfcc': mfcc.T
-        }
+        total_duration = len(y) / sr
+        segment_length = int(sr * segment_duration)
 
-        return features
+        features_list = []
+
+        for start in range(0, len(y), segment_length):
+            end = start + segment_length
+            if end <= len(y):
+                segment_features = extract_features_from_segment(y, sr, start, end)
+                all_features = segment_features
+                all_features['filename'] = os.path.basename(audio_file)
+                all_features['start'] = start / sr
+                all_features['end'] = end / sr
+                features_list.append(all_features)
+
+        return features_list
 
     except Exception as e:
         print(f"Error extracting features from {audio_file}: {e}")
-        return None
-
-# Function to create and save a spectrogram with increased Mel bands
-def create_spectrogram(audio_file, output_folder, sr=22050, n_mels=256, n_fft=2048, hop_length=512, fmax=8000, word_type="tree"):
-    try:
-        y, sr = librosa.load(audio_file, sr=sr)
-
-        # Apply a band-pass filter to focus on relevant frequencies (300 Hz to 3400 Hz)
-        y = librosa.effects.preemphasis(y)
-        y = librosa.effects.trim(y, top_db=30)[0]
-        S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels, n_fft=n_fft, hop_length=hop_length, fmax=fmax)
-        S_DB = librosa.power_to_db(S, ref=np.max)
-
-        plt.figure(figsize=(12, 6))
-        librosa.display.specshow(S_DB, sr=sr, x_axis=None, y_axis=None, fmax=fmax)
-        
-        plt.axis('off')
-        plt.gca().set_position([0, 0, 1, 1])  # Remove the whitespace
-
-        base_name = os.path.splitext(os.path.basename(audio_file))[0]
-        if word_type == "tree":
-            output_path = os.path.join(output_folder, f"tree_spectrogram_{base_name}.png")
-        else:
-            output_path = os.path.join(output_folder, f"three_spectrogram_{base_name}.png")
-        
-        plt.savefig(output_path, bbox_inches='tight', pad_inches=0)
-        plt.close()
-        print(f"Spectrogram saved to {output_path}")
-
-    except Exception as e:
-        print(f"Error creating spectrogram for {audio_file}: {e}")
-
-# Main function to process all audio files in a folder
-def process_audio_folder(folder_path, output_folder, sr=22050, n_mels=256, n_fft=2048, hop_length=256, fmax=8000, word_type="tree"):
-    all_features = []
-    filenames = []
-
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.wav') or filename.endswith('.mp3'):
-            file_path = os.path.join(folder_path, filename)
-            if filename.endswith('.mp3'):
-                try:
-                    file_path = convert_mp3_to_wav(file_path)
-                except Exception as e:
-                    print(f"Error converting {file_path} to WAV: {e}")
-                    continue
-
-            features = extract_features(file_path, sr=sr)
-            if features:
-                all_features.append(features)
-                filenames.append(filename)
-                create_spectrogram(file_path, output_folder, sr=sr, n_mels=n_mels, n_fft=n_fft, hop_length=hop_length, fmax=fmax, word_type=word_type)
+        return []
     
-    return all_features, filenames
+#list of words
+words = ["three", "tree"]
 
-# Function to process word data
-def process_word_data(word_folder_path, spectrogram_output_folder, csv_output_file, sr=22050, fmax=8000, word_type="tree"):
-    # Ensure the output folders exist
-    os.makedirs(spectrogram_output_folder, exist_ok=True)
-    os.makedirs(os.path.dirname(csv_output_file), exist_ok=True)
+#base folder containing word subfolders
+#base_folder_path = '/Users/simonrisk/Desktop/speech_therapy/archive/augmented_dataset/augmented_dataset'
+base_folder_path = '/Users/simonrisk/Desktop/speech_therapy/external_data/'
 
-    # Process the specified word folder with increased Mel bands
-    print(f"Processing word folder: {word_folder_path}")
-    all_features, filenames = process_audio_folder(word_folder_path, spectrogram_output_folder, sr=sr, n_mels=256, n_fft=2048, hop_length=256, fmax=fmax, word_type=word_type)
+#function to process a folder of audio files
+def process_audio_folder(folder_path, word_label):
+    results = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.wav'):
+            file_path = os.path.join(folder_path, filename)
+            features_list = extract_features(file_path)
+            for features in features_list:
+                features['word'] = word_label
+                results.append(features)
+    return results
 
-    # Write results to a CSV file
-    print("Writing results to CSV file...")
 
-    # Flattening and creating DataFrame
-    flattened_data = []
-    for idx, features in enumerate(all_features):
-        num_frames = features['mfcc'].shape[0]
-        for frame in range(num_frames):
-            frame_data = {key: features[key][frame] for key in features}
-            frame_data['filename'] = filenames[idx]
-            flattened_data.append(frame_data)
+all_results = []
 
-    df = pd.DataFrame(flattened_data)
-    df.to_csv(csv_output_file, index=False)
-    print("CSV file generation completed.")
+for word in words:
+    print(f"Processing word: {word}")
+    folder_path = os.path.join(base_folder_path, word)
+    word_results = process_audio_folder(folder_path, word)
+    all_results.extend(word_results)
+    print(f"Completed processing word: {word}")
 
-    # Indicate completion
-    print("Feature extraction, spectrogram generation, and CSV generation completed.")
+print("Writing results to CSV file...")
+df = pd.DataFrame(all_results)
+csv_file_path = '/Users/simonrisk/Desktop/speech_therapy/file_features.csv'
+df.to_csv(csv_file_path, index=False)
+print("CSV file generation completed.")
 
-# Specify paths for "tree"
-tree_word_folder_path = '/Users/simonrisk/Desktop/speech_therapy_1/archive/augmented_dataset/augmented_dataset/tree/'
-tree_spectrogram_output_folder = '/Users/simonrisk/Desktop/speech_therapy_1/spectrograms/tree'
-tree_csv_output_file = '/Users/simonrisk/Desktop/speech_therapy_1/csvs/tree.csv'
+df.head()
 
-# Specify paths for "three"
-three_word_folder_path = '/Users/simonrisk/Desktop/speech_therapy_1/archive/augmented_dataset/augmented_dataset/three/'
-three_spectrogram_output_folder = '/Users/simonrisk/Desktop/speech_therapy_1/spectrograms/three'
-three_csv_output_file = '/Users/simonrisk/Desktop/speech_therapy_1/csvs/three.csv'
+#get summary statistics
+df.describe(include='all')
 
-# Desired fmax value and sampling rate
-desired_fmax = 8000  # Adjust this value as needed
-desired_sr = 88200  # Set the desired sampling rate #was half of this
-
-# Process "tree" data
-process_word_data(tree_word_folder_path, tree_spectrogram_output_folder, tree_csv_output_file, sr=desired_sr, fmax=desired_fmax, word_type="tree")
-
-# Process "three" data
-process_word_data(three_word_folder_path, three_spectrogram_output_folder, three_csv_output_file, sr=desired_sr, fmax=desired_fmax, word_type="three")
